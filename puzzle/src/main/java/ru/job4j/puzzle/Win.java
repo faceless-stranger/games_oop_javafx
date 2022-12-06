fad0b6986eb5b6a652d930912e525bd743e7acb8
PC <faceless-stranger@mail.ru>
2022-12-06 21:35:36 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,45 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = vreticalWIn(board) || horizontalWIn(board);
         return rsl;
+    }
+
+    public static boolean vreticalWIn(int[][] board) {
+        boolean vertical = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                for (int j = 0; j < board.length; j++) {
+                   if (board[j][i] != 1) {
+                   return false;
+                   }
+                }
+            }
+        }
+        return vertical;
+    }
+
+    public static boolean horizontalWIn(int[][] board) {
+        boolean horiz = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] != 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return horiz;
+    }
+    public static void main(String[] args) {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        System.out.println(vreticalWIn(board));
     }
 }
