23355d4941c47565dd524bfbed15630b9d389f17
PC <faceless-stranger@mail.ru>
2022-12-09 13:09:23 +0300

    Исправил

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,36 +1,38 @@
 package ru.job4j.puzzle;
 
 public class Win {
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board[column].length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = monoVertical(board) || monoHorizontal(board);
-        return rsl;
-    }
-
-    public static boolean monoVertical(int[][] board) {
-        boolean vertical = true;
+        boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                for (int j = 0; j < board.length; j++) {
-                   if (board[j][i] != 1) {
-                   return false;
-                   }
-                }
+            if ((board[i][i] == 1
+                    && monoHorizontal(board, i)
+                    || monoVertical(board, i))) {
+                result = true;
+                break;
             }
         }
-        return vertical;
-    }
-
-    public static boolean monoHorizontal(int[][] board) {
-        boolean horizontal = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                for (int j = 0; j < board.length; j++) {
-                    if (board[i][j] != 1) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return horizontal;
+        return result;
     }
 }
