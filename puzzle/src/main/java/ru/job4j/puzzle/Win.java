a09d3cd76b2dc24b8282a13b135dc447316fd99e
PC <faceless-stranger@mail.ru>
2022-12-13 13:24:34 +0300

    Исправил

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -26,9 +26,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if ((board[i][i] == 1
-                    && monoHorizontal(board, i)
-                    || monoVertical(board, i))) {
+            if (board[i][i] == 1 && (monoVertical(board, i) || monoHorizontal(board, i))) {
                 result = true;
                 break;
             }
