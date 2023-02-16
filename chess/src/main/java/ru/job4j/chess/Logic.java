b871b79b95c264f3b58db2acff19467dc6307aa1
PC <faceless-stranger@mail.ru>
2023-02-16 23:31:52 +0300

    Каркас шахматной доски

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -23,7 +23,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell cell : steps) {
-                if(figure != null && figure.position().equals(cell)){
+                if (figure != null && figure.position().equals(cell)) {
                     throw new OccupiedCellException();
                 }
             }
